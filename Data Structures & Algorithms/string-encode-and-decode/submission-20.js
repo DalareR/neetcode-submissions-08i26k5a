75243e642737e96abc17a0782e74d8092525cb27
Dalare Rana <103294114+DalareR@users.noreply.github.com>
2026-04-30 13:03:49 -0400

    Add: string-encode-and-decode - submission-20

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-20.js
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    encode(strs) {
+        return strs.map(str => str.length + "#" + str).join(""); // modify str to add word length with # and join them together
+    }
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    decode(str) {
+        const result = [];
+        let i = 0;
+
+        while (i < str.length) {
+            const j = str.indexOf("#", i);      // find '#' from position i
+            const wordLength = Number(str.slice(i, j));  // length is between i and j
+            const startIndex = j + 1;
+            const endIndex = wordLength + startIndex;
+            const word = str.slice(startIndex, endIndex); // start after '#', end at ???
+            
+            result.push(word);
+            
+            i = endIndex;                            // move i to the start of next encoded word
+        }
+
+        return result;
+    }
+}
