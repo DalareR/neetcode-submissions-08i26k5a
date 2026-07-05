1e75b05453e82c7c04d3c83a2135a0bd4ee0fac2
Dalare Rana <103294114+DalareR@users.noreply.github.com>
2026-07-05 13:33:50 -0400

    Add: string-encode-and-decode - submission-27

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-27.js
@@ -0,0 +1,40 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    encode(strs) {
+        const result = [];
+        
+        for(const str of strs) {
+            const strLen = str.length;
+            
+            result.push(`${strLen}#${str}`);
+        }
+        
+        return result.join("")
+    }
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    decode(str) {
+        let initialIndex = 0;
+        const result = [];
+        
+        while(initialIndex < str.length) {
+            const hashIndex = str.indexOf('#', initialIndex);
+            const wordLen = Number(str.slice(initialIndex,hashIndex));
+            const startIndex = hashIndex + 1;
+            const endIndex = startIndex + wordLen;
+            const word = str.slice(startIndex, endIndex);
+
+            result.push(word);
+            
+            initialIndex = endIndex;
+        }
+        
+        return result;
+    }
+}
